224d842b72d382ec0f81bab8647aced246571365
Mixa Site <mixasite23@gmail.com>
2024-10-10 23:10:12 -0700

    add packages/web

---
+++ packages/web/src/index.ts
@@ -0,0 +1,3 @@
+import GenerativeMenuSwitch from "./components/tailwind/generative/generative-menu-switch";
+import {defaultExtensions} from "./components/tailwind/extensions";
+export {GenerativeMenuSwitch, defaultExtensions}
---
+++ packages/web/tsup.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig, Options } from "tsup";
+
+export default defineConfig((options: Options) => ({
+  entry: {
+    index: "src/index.ts",
+  },
+  banner: {
+    js: "'use client'",
+  },
+  minify: true,
+
+  format: ["cjs", "esm"],
+  dts: true,
+  clean: true,
+
+  external: ["react", "react-dom"],
+  ...options,
+}));
